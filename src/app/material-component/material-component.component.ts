8cd9877d54280150ba1aa918e3cd6be89aaf364b
Naser Afsar <n.afsar@gmail.com>
2019-11-13 19:55:52 -0700

    password required

--- src/app/material-component/material-component.component.ts
+++ src/app/material-component/material-component.component.ts
@@ -1,9 +1,16 @@
 
-import {Component, OnInit, VERSION} from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 export class MaterialMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export class MaterialMatcherPassword implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
@@ -15,21 +22,26 @@ export class MaterialMatcher implements ErrorStateMatcher {
   templateUrl: './material-component.component.html',
   styleUrls: ['./material-component.component.css']
 })
-export class MaterialComponent implements OnInit{
+export class MaterialComponent implements OnInit {
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
-  
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
 
   version = VERSION.full;
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
-   errorMatcher = new MaterialMatcher();
+  errorMatcher = new MaterialMatcher();
+  errorMatcherPassword = new MaterialMatcherPassword();
 }
 
 
